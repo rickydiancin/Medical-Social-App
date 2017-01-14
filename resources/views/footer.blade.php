e4e6225510a93f1219fc0ea441dd6d3465d8156f
BattleGuard <kenneth.valladolid@ama.edu.ph>
2017-01-14 16:45:10 +0800

    Added laravel framework together with AdminLTE template

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,25 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+		<!-- To the right -->
+		<div class="pull-right hidden-xs">
+				Anything you want
+		</div>
+		<!-- Default to the left -->
+		<strong>Copyright © 2015 <a href="#">Company</a>.</strong> All rights reserved.
+</footer>
+
+
+    </div><!-- ./wrapper -->
+    <!-- REQUIRED JS SCRIPTS -->
+    <!-- jQuery 2.1.3 -->
+    <script src="{{ asset("/bower_components/AdminLTE/plugins/jQuery/jQuery-2.2.3.min.js")}}"></script>
+    <!-- Bootstrap 3.3.2 JS -->
+    <script src="{{ asset("/bower_components/AdminLTE/bootstrap/js/bootstrap.min.js")}}" type="text/javascript"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ asset("/bower_components/AdminLTE/dist/js/app.min.js")}}" type="text/javascript"></script>
+
+    <!-- Optionally, you can add Slimscroll and FastClick plugins.
+          Both of these plugins are recommended to enhance the
+          user experience -->
+    </body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('admin_template')
+@section('content')
+<div class='row'>
+		<div class='col-md-6'>
+
+		</div>
+</div><!-- /.row -->
+@endsection
